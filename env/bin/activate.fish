e34fdeab0dfd5d4adb543eaf8246b27b9c3de334
sam <samuelmotari54@gmail.com>
2024-11-21 04:52:39 +0300

    static files

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/san/Projects/ivy-league/backend/env"
+set -gx VIRTUAL_ENV "/home/san/Projects/ivy-league-api/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
