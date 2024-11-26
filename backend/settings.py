5ef4c57a8ce01a6a0e33ea2ede705b7e74df9db9
sam <samuelmotari54@gmail.com>
2024-11-26 05:09:40 +0300

    timedelta

--- backend/settings.py
+++ backend/settings.py
@@ -36,8 +36,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=365 * 100),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=365 * 100),
 }
 
 AUTH_USER_MODEL = 'api.user'
